7a4e39a63be9b01918e6345ff22e83f05845425d
DarkSoulEngineer <milchiscatalin@gmail.com>
2025-11-16 13:43:11 +0200

    DOSBox-X 2D Draw App

---
+++ application/build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+.\tasm.exe %1.asm
+
+.\tlink.exe %1.obj
---
+++ application/clean.bat
@@ -0,0 +1,10 @@
+@echo off
+REM ==========================================
+REM Clean all .OBJ and .MAP files in current folder
+REM ==========================================
+
+echo Cleaning .OBJ and .MAP files...
+del *.OBJ /Q
+del *.MAP /Q
+
+echo Done.
